3758b5a1d4c316ed83a04089cef68e3276f64540
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-10-22 17:48:42 +0800

    feat: ✨ add partytown

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite';
-import { qwikVite } from '@builder.io/qwik/optimizer';
-import { qwikCity } from '@builder.io/qwik-city/vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "vite";
+import { qwikVite } from "@builder.io/qwik/optimizer";
+import { qwikCity } from "@builder.io/qwik-city/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { partytownVite } from "@builder.io/partytown/utils";
+import { join } from "path";
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [
+      qwikCity(),
+      qwikVite(),
+      tsconfigPaths(),
+      partytownVite({ dest: join(__dirname, "public", "~partytown") }),
+    ],
   };
 });
