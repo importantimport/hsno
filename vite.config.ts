413c2914ff5d903f8710504c7585f4501164c70a
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-10-23 12:19:44 +0800

    refactor(partytown): ♻️ replace join with resolve

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { qwikVite } from '@builder.io/qwik/optimizer'
 import { qwikCity } from '@builder.io/qwik-city/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { partytownVite } from '@builder.io/partytown/utils'
-import { join } from 'path'
+import { resolve } from 'path'
 
 import { config as hsnoConfig } from './hsno.config'
 
@@ -15,6 +15,6 @@ export default defineConfig({
     }),
     qwikVite(),
     tsconfigPaths(),
-    partytownVite({ dest: join(__dirname, 'public', '~partytown') })
+    partytownVite({ dest: resolve('public', '~partytown') })
   ]
 })
