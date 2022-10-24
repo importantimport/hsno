aae05f6c5ede344caae5eae9a4c03eb25d153a85
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-10-24 17:51:53 +0800

    fix(entry): 🐛 fix origin

--- src/entry.static.tsx
+++ src/entry.static.tsx
@@ -6,6 +6,6 @@ import { config as hsnoConfig } from '~/../hsno.config'
 
 // Execute Qwik City Static Site Generator
 qwikCityGenerate(render, {
-  origin: new URL(hsnoConfig.url).pathname,
+  origin: new URL(hsnoConfig.url).origin,
   outDir: resolve('dist')
 })
