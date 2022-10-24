3ae7e56762769b85f4ce350d188207c7b8f14fd8
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-10-24 17:50:15 +0800

    refactor(entry): ♻️ replace join with resolve

--- src/entry.static.tsx
+++ src/entry.static.tsx
@@ -1,12 +1,11 @@
 import { qwikCityGenerate } from '@builder.io/qwik-city/static/node'
 import render from '~/entry.ssr'
-import { fileURLToPath } from 'url'
-import { join } from 'path'
+import { resolve } from 'path'
 
 import { config as hsnoConfig } from '~/../hsno.config'
 
 // Execute Qwik City Static Site Generator
 qwikCityGenerate(render, {
   origin: new URL(hsnoConfig.url).pathname,
-  outDir: join(fileURLToPath(import.meta.url), '..', '..', 'dist')
+  outDir: resolve('dist')
 })
