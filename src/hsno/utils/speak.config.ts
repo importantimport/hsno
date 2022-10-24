fb3dab1c5f34c94d2ae42c681a6596185553fabc
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-10-24 23:43:12 +0800

    refactor(i18n): ♻️ use vite glob

--- src/hsno/utils/speak.config.ts
+++ src/hsno/utils/speak.config.ts
@@ -6,17 +6,16 @@ import type {
   TranslateFn
 } from 'qwik-speak'
 import { $ } from '@builder.io/qwik'
-import { isServer } from '@builder.io/qwik/build'
 import { config as hsnoConfig } from '~/../hsno.config'
 
 export const { i18n: config } = hsnoConfig
 
 export const loadTranslation$: LoadTranslationFn = $(
-  async (lang: string, asset: string, url?: URL) => {
-    if (import.meta.env.DEV)
-      return await fetch(
-        `${isServer && url ? url.origin : ''}/i18n/${lang}/${asset}.json`
-      ).then((res) => res.json())
+  async (lang: string, asset: string, _url?: URL) => {
+    if (import.meta.env.DEV) {
+      const assets = import.meta.glob('./hsno/i18n/**/*.json', { as: 'raw' })
+      return JSON.parse(await assets[`./hsno/i18n/${lang}/${asset}.json`]())
+    }
   }
 )
 
