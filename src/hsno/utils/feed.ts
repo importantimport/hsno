ceedff55e39c463f51cbdf936f4ce7189d8038cf
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-10-30 00:30:50 +0800

    refactor(feed): 🔥 remove stringify

--- src/hsno/utils/feed.ts
+++ src/hsno/utils/feed.ts
@@ -1,43 +1,40 @@
 import { renderToString } from '@builder.io/qwik/server'
 import { config as hsnoConfig } from '~/../hsno.config'
 
-export const toJSONFeed = async (posts: Hsno.Post[]) =>
-  JSON.stringify(
+export const toJSONFeed = async (posts: Hsno.Post[]) => ({
+  version: 'https://jsonfeed.org/version/1.1',
+  title: 'Hsno',
+  home_page_url: hsnoConfig.url,
+  feed_url: new URL('feed.json', hsnoConfig.url).toString(),
+  description: "!mportantImport's Next-Gen Blog Starter.",
+  icon: new URL('favicon.svg', hsnoConfig.url).toString(),
+  favicon: new URL('favicon.svg', hsnoConfig.url).toString(),
+  authors: [
     {
-      version: 'https://jsonfeed.org/version/1.1',
-      title: 'Hsno',
-      home_page_url: hsnoConfig.url,
-      feed_url: new URL('feed.json', hsnoConfig.url).toString(),
-      description: "!mportantImport's Next-Gen Blog Starter.",
-      icon: new URL('favicon.svg', hsnoConfig.url).toString(),
-      favicon: new URL('favicon.svg', hsnoConfig.url).toString(),
-      authors: [
-        {
-          name: 'John Doe',
-          url: new URL('~johndoe', hsnoConfig.url).toString(),
-          avatar: new URL('favicon.svg', hsnoConfig.url).toString()
-        }
-      ],
-      language: hsnoConfig.i18n.defaultLocale.lang,
-      // hubs: [{ type: 'WebSub', url: 'https://hub.hsno.local' }],
-      items: await Promise.all(
-        posts.map(async (post) => ({
-          id: post.slug,
-          url: new URL(post.slug, hsnoConfig.url).toString(),
-          title: post.title,
-          summary: post.summary,
-          image: post.image,
-          date_published: post.published ?? post.created,
-          date_modified: post.updated ?? post.published ?? post.created,
-          tags: post.tags,
-          content_html: (await renderToString(post.Content!(), {
-            containerTagName: 'body'
-          })).html
-        }))
-      )
-    },
-    null,
-    2
+      name: 'John Doe',
+      url: new URL('~johndoe', hsnoConfig.url).toString(),
+      avatar: new URL('favicon.svg', hsnoConfig.url).toString()
+    }
+  ],
+  language: hsnoConfig.i18n.defaultLocale.lang,
+  // hubs: [{ type: 'WebSub', url: 'https://hub.hsno.local' }],
+  items: await Promise.all(
+    posts.map(async (post) => ({
+      id: post.slug,
+      url: new URL(post.slug, hsnoConfig.url).toString(),
+      title: post.title,
+      summary: post.summary,
+      image: post.image,
+      date_published: post.published ?? post.created,
+      date_modified: post.updated ?? post.published ?? post.created,
+      tags: post.tags,
+      content_html: (
+        await renderToString(post.Content!(), {
+          containerTagName: 'body'
+        })
+      ).html
+    }))
   )
+})
 
 // export const toJF2Feed = (posts: Hsno.Post[]) => {}
