1d8321831cb77c9653ba8b4cb7bcd3d1481c2467
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-10-30 00:34:13 +0800

    refactor(feed): ♻️ set content type

--- src/routes/(files)/feed.json/index.ts
+++ src/routes/(files)/feed.json/index.ts
@@ -2,5 +2,8 @@ import type { RequestHandler } from '@builder.io/qwik-city'
 import { getPostsContent } from '~/hsno/utils/posts'
 import { toJSONFeed } from '~/hsno/utils/feed'
 
-export const onGet: RequestHandler = async () =>
-  await toJSONFeed(await getPostsContent())
+export const onGet: RequestHandler = async ({ response }) =>
+  response.headers.set(
+    'Content-Type',
+    'application/feed+json; charset=utf-8'
+  )! && (await toJSONFeed(await getPostsContent()))
