3c815d1df00938f0390c6f54a8edd21927155c24
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-10-29 21:01:12 +0800

    refactor(types): 🏷️ url ends with slash

--- hsno.config.ts
+++ hsno.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, locales } from './src/hsno/utils/hsno.config'
 
 export const config: Hsno.Config = defineConfig({
-  url: 'https://hsno.local',
+  url: 'https://hsno.local/',
   i18n: {
     defaultLocale: locales['en-US']
   }
--- src/hsno/types/config.ts
+++ src/hsno/types/config.ts
@@ -2,13 +2,13 @@ import type { SpeakConfig } from 'qwik-speak'
 import type { PartytownConfig } from '@builder.io/partytown/integration'
 
 export type HsnoConfig = {
-  url: string
+  url: `${string}/`
   i18n: SpeakConfig
   partytown?: PartytownConfig
 }
 
 export type HsnoUserConfig = {
-  url?: string
+  url?: `${string}/`
   i18n?: Partial<SpeakConfig>
   partytown?: PartytownConfig
 }
