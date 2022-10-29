2c92a90b3dffa4f85f3c59e8df8ce1499a8029de
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-10-29 21:43:24 +0800

    refactor(i18n): ♻️ improve load translation

--- src/hsno/utils/speak.config.ts
+++ src/hsno/utils/speak.config.ts
@@ -1,8 +1,8 @@
 import type {
   LoadTranslationFn,
   ResolveLocaleFn,
-//   SpeakLocale,
-//   StoreLocaleFn,
+  //   SpeakLocale,
+  //   StoreLocaleFn,
   TranslateFn
 } from 'qwik-speak'
 import { $ } from '@builder.io/qwik'
@@ -11,12 +11,13 @@ import { config as hsnoConfig } from '~/../hsno.config'
 export const { i18n: config } = hsnoConfig
 
 export const loadTranslation$: LoadTranslationFn = $(
-  async (lang: string, asset: string) => {
-    if (import.meta.env.DEV) {
-      const assets = import.meta.glob('./hsno/i18n/**/*.json', { as: 'raw' })
-      return JSON.parse(await assets[`./hsno/i18n/${lang}/${asset}.json`]())
-    }
-  }
+  async (lang: string, asset: string) =>
+    import.meta.env.DEV &&
+    JSON.parse(
+      import.meta.glob('./hsno/i18n/**/*.json', { as: 'raw', eager: true })[
+        `./hsno/i18n/${lang}/${asset}.json`
+      ]
+    )
 )
 
 export const resolveLocale$: ResolveLocaleFn = $((url?: URL) => {
@@ -34,6 +35,6 @@ export const resolveLocale$: ResolveLocaleFn = $((url?: URL) => {
 
 export const translateFn: TranslateFn = {
   loadTranslation$: loadTranslation$,
-  resolveLocale$: resolveLocale$,
-//   storeLocale$: storeLocale$
+  resolveLocale$: resolveLocale$
+  //   storeLocale$: storeLocale$
 }
