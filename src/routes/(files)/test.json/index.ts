cac88910e2e794ba4d52bcd915a38b5e7fc63066
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-11-01 01:30:29 +0800

    refactor(test.json): ♻️ legacy format

--- src/routes/(files)/test.json/index.ts
+++ src/routes/(files)/test.json/index.ts
@@ -1,6 +1,8 @@
 import type { RequestHandler } from '@builder.io/qwik-city'
 
-export const onGet: RequestHandler = async ({ response }) =>
-  response.headers.set('Content-Type', 'application/json; charset=utf-8')! && {
+export const onGet: RequestHandler = async ({ response }) => {
+  response.headers.set('Content-Type', 'application/json; charset=utf-8')
+  return {
     mode: import.meta.env.MODE
   }
+}
