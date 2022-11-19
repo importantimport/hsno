2bba089ce911feab6f8339d47ff5b45be0a9d9ba
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-11-20 00:05:14 +0800

    feat: ✨ imagetools

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { imagetools } from 'vite-imagetools'
 import { qwikCity } from '@builder.io/qwik-city/vite'
 import { qwikVite } from '@builder.io/qwik/optimizer'
 import { qwikSpeakInline } from 'qwik-speak/inline'
@@ -9,10 +10,12 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import { resolve } from 'node:path'
 
 import { config as hsnoConfig } from './hsno.config'
+import { config as imagetoolsConfig } from './src/hsno/config/imagetools.config'
 import { config as pwaConfig } from './src/hsno/config/pwa.config'
 
 export default defineConfig({
   plugins: [
+    imagetools(imagetoolsConfig),
     qwikCity({
       basePathname: new URL(hsnoConfig.url).pathname
     }),
