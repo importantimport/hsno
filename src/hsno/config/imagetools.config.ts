7e195d889a4431511685f6f39b48de06ab9b2aee
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-11-20 12:58:48 +0800

    refactor(imagetools.config): 🔥 disable directives

--- src/hsno/config/imagetools.config.ts
+++ src/hsno/config/imagetools.config.ts
@@ -1,9 +1,9 @@
 import type { imagetools } from 'vite-imagetools'
 
 export const config: Partial<Parameters<typeof imagetools>[0]> = {
-  defaultDirectives: (_url) =>
-    new URLSearchParams({
-      width: ['480', '1024', '1920'].join(';'),
-      format: ['avif', 'webp'].join(';')
-    })
+  //   defaultDirectives: (_url) =>
+  //     new URLSearchParams({
+  //       width: ['480', '1024', '1920'].join(';'),
+  //       format: ['avif', 'webp'].join(';')
+  //     })
 }
