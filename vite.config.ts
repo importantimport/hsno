f92144fbb16975397c20a83b9abcfad231985a4b
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-11-21 21:03:53 +0800

    chore: setup lightningcss

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  build: { cssMinify: 'lightningcss' },
+  css: { transformer: 'lightningcss' },
   plugins: [
     qwikCity(),
     qwikVite(),
