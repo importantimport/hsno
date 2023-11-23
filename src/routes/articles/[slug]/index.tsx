3dc05d22753d61570372cfe946bd235117f4ce0c
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-11-23 19:56:01 +0800

    refactor(articles): use route loader

--- src/routes/articles/[slug]/index.tsx
+++ src/routes/articles/[slug]/index.tsx
@@ -1,25 +1,38 @@
 import { component$ } from '@builder.io/qwik'
-import { type StaticGenerateHandler, useLocation } from '@builder.io/qwik-city'
+import { type DocumentHead, type StaticGenerateHandler, routeLoader$ } from '@builder.io/qwik-city'
 import { allArticles } from 'contentlayer/generated'
 
-export default component$(() => {
-  const { params } = useLocation()
+export const useArticle = routeLoader$(async ({ params }) => {
   const article = allArticles.find(({ slug }) => slug === params.slug)
   if (!article)
     throw new Error(`Article not found for slug: ${params.slug}`)
 
+  return article
+})
+
+export default component$(() => {
+  const article = useArticle()
+
   return (
     <article>
-      <h1>{article.title}</h1>
-      <time dateTime={article.created}>
+      <h1>{article.value.title}</h1>
+      <time dateTime={article.value.created}>
         {new Intl.DateTimeFormat('en-US', { // TODO: hsno.config.ts site.lang
           dateStyle: 'full',
-        }).format(new Date(article.created))}
+        }).format(new Date(article.value.created))}
       </time>
-      <div dangerouslySetInnerHTML={article.body.html}></div>
+      <div dangerouslySetInnerHTML={article.value.body.html}></div>
     </article>
   )
 })
+
+export const head: DocumentHead = ({ resolveValue }) => {
+  const article = resolveValue(useArticle)
+
+  return {
+    title: `${article.title} - Hsno`,
+  }
+}
 
 export const onStaticGenerate: StaticGenerateHandler = () => ({
   params: allArticles.map(({ slug }) => ({ slug })),
